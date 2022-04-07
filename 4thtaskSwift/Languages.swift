9c553869eabef5a2f7049dccb0d1ee2801b9cbca
zacepilkina <zacepilina02_00@mail.ru>
2022-04-07 23:53:22 +0300

    ready to be checked

---
+++ 4thtaskSwift/Languages.swift
@@ -0,0 +1,43 @@
+//
+//  Languages.swift
+//  4thtaskSwift
+//
+//  Created by Anastasia on 04.04.2022.
+//
+
+import Foundation
+
+protocol Translation {
+    var tie: String {get set}
+    var win: String {get set}
+    var computer: String {get set}
+    var gameTitle: String {get set}
+    var settingsTitle: String {get set}
+    var cubeLabel: String {get set}
+}
+
+
+enum Languages: String, CaseIterable {
+    case english = "Eng"
+    case russian = "Rus"
+}
+
+
+struct EnglishLanguage: Translation {
+    var tie: String = "Tie"
+    var win: String = "U win"
+    var computer: String = "Computer wins"
+    var gameTitle: String = "Game"
+    var settingsTitle: String = "Settings"
+    var cubeLabel: String = "Start"
+}
+
+
+struct RussianLanguage: Translation {
+    var tie: String = "Ничья"
+    var win: String = "Ты выиграл"
+    var computer: String = "Компьютер выиграл"
+    var gameTitle: String = "Игра"
+    var settingsTitle: String = "Настройки"
+    var cubeLabel: String = "Старт"
+}
